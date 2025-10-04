51e916015ad358288c5ec3a50fbcb00227f54ceb
Ambuj <awesomeambuj@gmail.com>
2025-10-04 18:11:58 +0530

    CRUD operations performed, Controller and Service Layer used out of CSR, Repository not used yet

--- src/main/java/com/springweb/javaWebApp/controller/ProdController.java
+++ src/main/java/com/springweb/javaWebApp/controller/ProdController.java
@@ -3,8 +3,8 @@ package com.springweb.javaWebApp.controller;
 import com.springweb.javaWebApp.model.Product;
 import com.springweb.javaWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,5 +19,25 @@ public class ProdController {
     @RequestMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
+    }
+
+    @RequestMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return service.getProdId(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProductToCart(@RequestBody Product prod){
+          service.addProd(prod);
+    }
+
+    @PutMapping("/products")
+    public void editProduct(@RequestBody Product prod){
+        service.updateProduct(prod);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public String deleteProduct(@PathVariable int prodId){
+        return service.deleteProd(prodId);
     }
 }
--- src/main/java/com/springweb/javaWebApp/service/ProductService.java
+++ src/main/java/com/springweb/javaWebApp/service/ProductService.java
@@ -12,12 +12,47 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> prodArr = Arrays.asList(
+    List<Product> prodArr = new ArrayList<>( Arrays.asList(
             new Product(501, "Samsung", 70000),
             new Product(502, "JBL", 8000),
             new Product(503, "Harry Potter Wand", 999000)
-    );
+    ));
     public List<Product> getProducts(){
            return prodArr;
+    }
+
+    public Product getProdId(int prodId){
+        return prodArr.stream()
+        .filter(p -> p.getProdId() == prodId)
+        .findFirst().orElse(new Product(0, "NO PRODUCT", 0));
+//      .findFirst().get();
+    }
+
+    public void addProd(Product prod){
+        prodArr.add(prod);
+    }
+
+    public void updateProduct(Product prod) {
+        int idx = 0;
+        for(int i=0;i<prodArr.size();i++){
+            if(prodArr.get(i).getProdId() == prod.getProdId()){
+                idx = i;
+                break;
+            }
+        }
+        prodArr.set(idx, prod);
+    }
+
+    public String deleteProd(int prodId){
+        int idx = -1;
+        for(int i=0;i<prodArr.size();i++){
+            if(prodArr.get(i).getProdId() == prodId){
+                idx = i;
+                break;
+            }
+        }
+        if(idx == -1) return "Id not found...";
+        prodArr.remove(idx);
+        return "Id removed";
     }
 }
